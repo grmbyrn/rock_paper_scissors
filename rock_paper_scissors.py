b2f9f7e558e70242bbf119edc5efdcae38d67e8f
grmbyrn <graemebyrne_89@hotmail.com>
2023-10-12 12:54:01 +0100

    set playagain to True and place code in while loop, ask player if they want to play again with input, if yes then continue, if not playagain is False and sys.exit()

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -9,34 +9,42 @@ class RPS(Enum):
     SCISSORS = 3
 
 
-print('')
+playagain = True
 
-playerchoice = input(
-    'Enter...\n1 for Rock,\n2 for Paper, or\n3 for Scissors:\n\n'
-)
+while playagain:
+    playerchoice = input(
+        'Enter...\n1 for Rock,\n2 for Paper, or\n3 for Scissors:\n\n')
 
-player = int(playerchoice)
+    player = int(playerchoice)
 
-if player < 1 or player > 3:
-    sys.exit('You must enter 1, 2, or 3')
+    if player < 1 or player > 3:
+        sys.exit('You must enter 1, 2, or 3')
 
-computerchoice = random.choice('123')
+    computerchoice = random.choice('123')
 
-computer = int(computerchoice)
+    computer = int(computerchoice)
 
-print('')
-print('You chose ' + str(RPS(player)).replace('RPS.', '').title())
-print('Python chose ' + str(RPS(computer)).replace('RPS.', '').title())
+    print('')
+    print('You chose ' + str(RPS(player)).replace('RPS.', '').title())
+    print('Python chose ' + str(RPS(computer)).replace('RPS.', '').title())
 
-print('')
+    if player == 1 and computer == 3:
+        print('You win!')
+    elif player == 2 and computer == 1:
+        print('You win!')
+    elif player == 3 and computer == 2:
+        print('You win!')
+    elif player == computer:
+        print('Tie game!')
+    else:
+        print('Python wins!')
 
-if player == 1 and computer == 3:
-    print('You win!')
-elif player == 2 and computer == 1:
-    print('You win!')
-elif player == 3 and computer == 2:
-    print('You win!')
-elif player == computer:
-    print('Tie game!')
-else:
-    print('Python wins!')
+    playagain = input("\nPlay again? \nY for Yes or \nQ to Quit ")
+
+    if playagain.lower() == 'y':
+        continue
+    else:
+        print('\nThank you for playing!')
+        playagain = False
+
+sys.exit('Bye!')
